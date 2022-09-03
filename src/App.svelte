6851134afe2cd7b628d43e0b77e9ca370e918941
ntwariegide2 <ntwariegide2@gmail.com>
2022-09-03 23:14:35 +0200

    ft: implement states

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script>
 	export let name;
+
+	let rando;
+
+	function setRando() {
+		rando  = Math.random()
+	}
 </script>
 
 <main>
+	
 	<h1>Hello {name}!</h1>
+
+	<h2>Random : {rando}</h2>
+
+	<p>{ Math.round(rando) ? 'Winner ': 'Loser '}</p>
+
+	<button on:click={setRando}>GENERATE</button>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
